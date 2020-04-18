6f2fdcb997be545cfadb1ef2b0b1749f17111402
asyrofist <rakhasyofist@gmail.com>
2020-04-18 11:38:59 +0700

    ditambahkan

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
-<?php include "header.php" ?>
-<?php include "navigation.php" ?>
+<?php
+// Version
+define('VERSION', '3.0.2.0');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-<?php include "home.php" ?>
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
 
-<?php include "footer.php" ?>
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit;
+}
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
+
+start('catalog');
