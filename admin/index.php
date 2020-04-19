e71c2e0a72066df2222e015a1b5b8565ad0d0b2b
asyrofist <rakhasyrofist@gmail.com>
2020-04-19 10:30:26 +0700

    diubah

---
+++ admin/index.php
@@ -0,0 +1,42 @@
+<?php include "header.php" ?>
+<?php include "sidebar.php" ?>
+<?php include "topbar.php" ?>
+
+<?php
+if (isset($_GET['halaman'])) {
+  if ($_GET['halaman'] == 'main') {
+    include "main.php";
+  } elseif ($_GET['halaman'] == '404') {
+    include "404.php";
+  } elseif ($_GET['halaman'] == 'blank') {
+    include "blank.php";
+  } elseif ($_GET['halaman'] == 'buttons') {
+    include "buttons.php";
+  } elseif ($_GET['halaman'] == 'cards') {
+    include "cards.php";
+  } elseif ($_GET['halaman'] == 'charts') {
+    include "charts.php";
+  } elseif ($_GET['halaman'] == 'forgot-paassword') {
+    include "forgot-paassword.php";
+  } elseif ($_GET['halaman'] == 'login') {
+    include "login.php";
+  } elseif ($_GET['halaman'] == 'register') {
+    include "register.php";
+  } elseif ($_GET['halaman'] == 'tables') {
+    include "tables.php";
+  } elseif ($_GET['halaman'] == 'utilities-animation') {
+    include "utilities-animation.php";
+  } elseif ($_GET['halaman'] == 'utilities-border') {
+    include "utilities-border.php";
+  } elseif ($_GET['halaman'] == 'utilities-color') {
+    include "utilities-color.php";
+  } elseif ($_GET['halaman'] == 'utilities-other') {
+    include "utilities-other.php";
+  } else {
+    include "main.php";
+  }
+}
+?>
+
+<?php include "main.php" ?>
+<?php include "footer.php" ?>
