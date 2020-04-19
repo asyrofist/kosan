1dc3bf39f78a05355b77b6489cc1a04718aba5b5
asyrofist <rakhasyrofist@gmail.com>
2020-04-19 08:32:51 +0700

    diubah

--- header.php
+++ header.php
@@ -31,6 +31,7 @@
           <li class="nav-item"><a class="nav-link js-scroll-trigger" href="../#team">Team</a></li>
           <li class="nav-item"><a class="nav-link js-scroll-trigger" href="../#contact">Contact</a></li>
           <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?halaman=blog">blog</a></li>
+          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?halaman=shop">shop</a></li>
         </ul>
       </div>
     </div>
