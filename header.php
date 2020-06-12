328496787ee5c1d7195dec5665e7bcaf184e6ffa
asyrofist <rakhasyrofist@gmail.com>
2020-06-12 08:41:49 +0700

    diubah

--- header.php
+++ header.php
@@ -26,7 +26,7 @@ include "config.php";
   <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div class="container">
       <!-- <a class="navbar-brand js-scroll-trigger" href="../<?php echo $baseurl; ?>#page-top"><img src="dist/assets/img/navbar-logo.svg" /></a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fas fa-bars ml-1"></i></button> -->
-      <a class="navbar-brand js-scroll-trigger" href="../<?php echo $baseurl; ?>#page-top"></a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fas fa-bars ml-1"></i></button>
+      <a class="navbar-brand js-scroll-trigger" href="../<?php echo $baseurl; ?>#page-top">Kosan Asyrofi</a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fas fa-bars ml-1"></i></button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav text-uppercase ml-auto">
           <li class="nav-item"><a class="nav-link js-scroll-trigger" href="../<?php echo $baseurl; ?>#services">Services</a></li>
