be471c23c3b8445e86b522387551ce97ac26f4cc
asyrofist <rakhasyrofist@gmail.com>
2020-06-12 09:17:44 +0700

    diubah

--- admin/config.php
+++ admin/config.php
@@ -1,12 +1,12 @@
 <?php
 $host = "localhost";
-$username = "root";
-// $username = "kosan";
-$passwd = null;
-// $passwd = "@kosanAsyrofi1234";
+// $username = "root";
+$username = "kosan";
+// $passwd = null;
+$passwd = "@kosanAsyrofi1234";
 $dbname = "db_kosan";
 $port = null;
 $socket = null;
 $koneksi = new mysqli($host, $username, $passwd, $dbname, $port, $socket);
-// $baseurl = "https://kos.asyrofi.com/";
-$baseurl = "kosanbaru/";
+$baseurl = "../https://kos.asyrofi.com/";
+// $baseurl = "kosanbaru/";
